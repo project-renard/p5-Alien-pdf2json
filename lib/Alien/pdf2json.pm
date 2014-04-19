5c98c4c56cb7cf69120162f0d03de2e884752505
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-19 18:13:38 -0500

    init

---
+++ lib/Alien/pdf2json.pm
@@ -0,0 +1,29 @@
+package Alien::pdf2json;
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Alien::pdf2json - Alien package for the pdf2json PDF extraction library
+
+=head1 LICENSE
+
+pdf2json is licensed under GNU GPL v2.
+
+=head1 COPYRIGHT
+
+Copyright 2014 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=head1 SEE ALSO
+
+L<pdf2json|https://code.google.com/p/pdf2json/>
