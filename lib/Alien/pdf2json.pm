536bac88ea3872ca6a89dc576873342d6d6852fa
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-19 18:13:57 -0500

    Build results of 5c98c4c (on master)

---
+++ lib/Alien/pdf2json.pm
@@ -0,0 +1,54 @@
+package Alien::pdf2json;
+$Alien::pdf2json::VERSION = '0.001';
+use strict;
+use warnings;
+
+use parent 'Alien::Base';
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Alien::pdf2json
+
+=head1 VERSION
+
+version 0.001
+
+=head1 NAME
+
+Alien::pdf2json - Alien package for the pdf2json PDF extraction library
+
+=head1 LICENSE
+
+pdf2json is licensed under GNU GPL v2.
+
+=head1 COPYRIGHT
+
+Copyright 2014 Zakariyya Mughal.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License version 2.0.
+
+=head1 SEE ALSO
+
+L<pdf2json|https://code.google.com/p/pdf2json/>
+
+=head1 AUTHOR
+
+Zakariyya Mughal <zmughal@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Zakariyya Mughal.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
---
+++ t/use.t
@@ -0,0 +1,8 @@
+use Test::More;
+
+use strict;
+use warnings;
+
+use DDP; p system('pdf2json --help');
+
+done_testing;
