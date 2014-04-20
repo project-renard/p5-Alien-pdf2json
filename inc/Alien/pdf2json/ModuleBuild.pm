17ea5133a38545e255a7a64cbacdd999581ee2e5
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-19 20:31:41 -0500

    Build results of 5c98c4c (on master)

--- inc/Alien/pdf2json/ModuleBuild.pm
+++ inc/Alien/pdf2json/ModuleBuild.pm
@@ -7,21 +7,34 @@ use parent 'Alien::Base::ModuleBuild';
 use Archive::Extract;
 use File::Copy;
 use File::chdir;
+use File::Spec;
 
 
 sub alien_do_commands {
   my ($self, $phase) = @_;
+	my $dir = $self->config_data( 'working_directory' );
+	my $bin_dir = File::Spec->catfile( $dir, 'bin' );
+
+	if( $phase eq 'build' ) {
+			mkdir $bin_dir;
+	}
 	if( $self->is_windowsish() || 1 ) {
-		my $dir = $self->config_data( 'working_directory' );
-		# TODO extract inc/pdf2json-0.68-win32.zip
 		if( $phase eq 'build' ) {
-			use DDP; p $CWD;
-			use DDP; p $self->alien_library_destination;
-			die $CWD;
-			die $self->alien_library_destination;
-			# nop
+			# extract ../inc/pdf2json-0.68-win32.zip
+			my $win32_build_zip = glob File::Spec->catfile( $dir, '..', 'inc', '*-win32.zip' );
+			my $e = Archive::Extract->new( archive => $win32_build_zip );
+			$e->extract; 
+
+			# move from ./pdf2json.exe to ./bin/pdf2json.exe
+			move( File::Spec->catfile( $dir, 'pdf2json.exe'), File::Spec->catfile( $bin_dir,'pdf2json.exe') )
+
 			return 1;
 		} elsif(  $phase eq 'install' ) {
+			my $target_bin = File::Spec->catfile( $self->alien_library_destination, 'bin' );
+			my $target_pdf2json = File::Spec->catfile( $target_bin, 'pdf2json.exe' );
+			my $src_pdf2json = File::Spec->catfile( $bin_dir, 'pdf2json.exe' );
+			mkdir $target_bin;
+			copy( $src_pdf2json, $target_pdf2json );
 			return 1;
 		} else {
 			$self->SUPER::alien_do_commands($phase);
@@ -31,11 +44,6 @@ sub alien_do_commands {
 		$self->SUPER::alien_do_commands($phase);
 		if( $phase eq 'build' ) {
 			# after build
-			my $dir = $self->config_data( 'working_directory' );
-
-			my $bin_dir = File::Spec->catfile( $dir, 'bin' );
-			mkdir $bin_dir;
-			
 			my $pdf2json_src = File::Spec->catfile( $bin_dir, 'pdf2json' );
 			my $pdf2json_tgt = File::Spec->catfile( $dir, 'src', 'pdf2json' );
 
