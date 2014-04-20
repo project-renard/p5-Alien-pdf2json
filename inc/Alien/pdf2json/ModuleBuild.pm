9bbe2de292d7bbb95e75c97fc95181896bfdcdad
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-19 20:17:21 -0500

    Build results of 5c98c4c (on master)

--- inc/Alien/pdf2json/ModuleBuild.pm
+++ inc/Alien/pdf2json/ModuleBuild.pm
@@ -12,12 +12,14 @@ use File::chdir;
 sub alien_do_commands {
   my ($self, $phase) = @_;
 	use DDP; p $phase;
-	if( $self->is_windowsish || 1 ) {
+	if( $self->is_windowsish() || 1 ) {
 		my $dir = $self->config_data( 'working_directory' );
 		# TODO extract inc/pdf2json-0.68-win32.zip
 		if( $phase eq 'install' ) {
 			use DDP; p $CWD;
 			use DDP; p $self->alien_library_destination;
+		} else {
+			$self->SUPER::alien_do_commands($phase);
 		}
 	}
 	else {
