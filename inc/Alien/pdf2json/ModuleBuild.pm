303755e97587b606a4ff017694a7a135a17d7d9b
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-04-19 19:46:36 -0500

    Build results of 5c98c4c (on master)

---
+++ inc/Alien/pdf2json/ModuleBuild.pm
@@ -0,0 +1,13 @@
+package Alien::pdf2json::ModuleBuild;
+
+use strict;
+use warnings;
+
+use parent 'Alien::Base::ModuleBuild';
+use Archive::Extract;
+
+sub alien_do_commands {
+  my ($self, $phase) = @_;
+  use DDP; p $phase;
+  $self->SUPER::alien_do_commands($phase);
+}
